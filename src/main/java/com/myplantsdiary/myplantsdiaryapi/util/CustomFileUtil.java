8faaae3f85d260c39c5b1f567190da2e32380a08
dajeong <uwhami@gmail.com>
2024-05-28 22:45:59 +0900

    thumbnail 파일 생성 추가

--- src/main/java/com/myplantsdiary/myplantsdiaryapi/util/CustomFileUtil.java
+++ src/main/java/com/myplantsdiary/myplantsdiaryapi/util/CustomFileUtil.java
@@ -3,6 +3,7 @@ package com.myplantsdiary.myplantsdiaryapi.util;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,11 +46,19 @@ public class CustomFileUtil {
 
         List<String> uploadNames = new ArrayList<>();
         for(MultipartFile file : files){
-            String savedName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+            String savedName = UUID.randomUUID() + "_" + file.getOriginalFilename();
             Path savePath = Paths.get(uploadPath, savedName);
 
             try{
                 Files.copy(file.getInputStream(), savePath);
+
+                String contentType = file.getContentType();
+                //이미지 파일이라면
+                if(contentType != null || contentType.startsWith("image")){
+                    Path thumbnailPath = Paths.get(uploadPath, "s_" + savedName);
+                    Thumbnails.of(savePath.toFile()).size(200,200).toFile(thumbnailPath.toFile());
+                }
+
                 uploadNames.add(savedName);
             }catch(IOException e){
                 throw new RuntimeException(e);
