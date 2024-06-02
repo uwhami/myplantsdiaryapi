3ca2819483d1d5b3f08056d94f3bda17dd842802
dajeong <uwhami@gmail.com>
2024-06-02 23:53:31 +0900

    product update and delete

--- src/main/java/com/myplantsdiary/myplantsdiaryapi/repository/ProductRepository.java
+++ src/main/java/com/myplantsdiary/myplantsdiaryapi/repository/ProductRepository.java
@@ -1,7 +1,21 @@
 package com.myplantsdiary.myplantsdiaryapi.repository;
 
 import com.myplantsdiary.myplantsdiaryapi.domain.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @EntityGraph(attributePaths = "imageList")
+    @Query("select p from Product p where p.pno = :pno")
+    Optional<Product> selectOne(@Param("pno") Long pno);
+
+    @Modifying
+    @Query("update Product p set p.delFlag = :delFlag where p.pno = :pno")
+    void updateToDelete(@Param("delFlag") boolean delFlag, @Param("pno") Long pno);
 }
--- src/test/java/com/myplantsdiary/myplantsdiaryapi/repository/ProductRepositoryTests.java
+++ src/test/java/com/myplantsdiary/myplantsdiaryapi/repository/ProductRepositoryTests.java
@@ -1,11 +1,15 @@
 package com.myplantsdiary.myplantsdiaryapi.repository;
 
 import com.myplantsdiary.myplantsdiaryapi.domain.Product;
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.test.annotation.Commit;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
@@ -22,4 +26,49 @@ public class ProductRepositoryTests {
         product.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
         productRepository.save(product);
     }
+
+    @Transactional
+    @Test
+    public void testRead1(){
+        Long pno = 1L;
+        Optional<Product> result = productRepository.findById(pno);
+        Product product = result.orElseThrow();
+
+        log.info(product);
+        log.info(product.getPname());   //쿼리를 두 번 날리기 때문에 transactional 안하면 에러 발생.
+    }
+
+    @Test
+    public void testRead2(){
+        Long pno = 1L;
+        Optional<Product> result = productRepository.selectOne(pno);    //조인해서 쿼리를 한번만 날림.
+        Product product = result.orElseThrow();
+
+        log.info(product);
+        log.info(product.getPname());
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDelete(){
+        Long pno = 2L;
+        productRepository.updateToDelete(true, pno);
+    }
+
+    @Test
+    public void testUpdate(){
+        Product product = productRepository.selectOne(1L).get();
+
+        product.setPrice(3000);
+        //jpa 에서 collection을 사용 시에 기존에 물고 있는 collection을 사용해야한다.
+        product.clearList();
+
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE1.jpg");
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE2.jpg");
+        product.addImageString(UUID.randomUUID() + "_" + "PIMAGE3.jpg");
+
+        productRepository.save(product);
+    }
+
 }
