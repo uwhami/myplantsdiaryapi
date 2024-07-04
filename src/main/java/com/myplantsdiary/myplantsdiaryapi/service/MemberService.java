4bbf35d3b5187b5ce3dae81ff1178775632a1c51
dajeong <uwhami@gmail.com>
2024-07-04 23:22:10 +0900

    social sign in member insert!

--- src/main/java/com/myplantsdiary/myplantsdiaryapi/service/MemberService.java
+++ src/main/java/com/myplantsdiary/myplantsdiaryapi/service/MemberService.java
@@ -1,11 +1,26 @@
 package com.myplantsdiary.myplantsdiaryapi.service;
 
+import com.myplantsdiary.myplantsdiaryapi.domain.Member;
 import com.myplantsdiary.myplantsdiaryapi.dto.MemberDTO;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @Transactional
 public interface MemberService {
 
     MemberDTO getKakaoMember(String accessToken);
+
+    default MemberDTO entityToDTO(Member member) {
+
+        MemberDTO dto = new MemberDTO(
+                member.getEmail(),
+                member.getPassword(),
+                member.getNickname(),
+                member.isSocial(),
+                member.getMemberRoleList().stream().map(memberRole -> memberRole.name()).collect(Collectors.toList()));
+
+        return dto;
+    }
 
 }
